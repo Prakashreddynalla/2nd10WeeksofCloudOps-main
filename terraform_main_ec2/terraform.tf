a31e50edab5d083771fd122a27f84a44d6f8cced
Nalla Prakash reddy <prakashreddynalla285@gmail.com>
2025-01-18 13:14:25 +0530

    Update terraform.tf

--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -9,11 +9,11 @@ terraform {
   backend "s3" {
     bucket = "nareshawsdevvvv"
     key    = "ec2/terraform.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
 }
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
