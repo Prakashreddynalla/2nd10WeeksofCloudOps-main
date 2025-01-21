4c1241a99c6e39748dbbc42af132cfdf1d20051e
Nalla Prakash reddy <prakashreddynalla285@gmail.com>
2025-01-21 15:12:23 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://a9c1daf74e2964022bc65514afb5c4e0-1655992063.us-west-1.elb.amazonaws.com";
+const API_BASE_URL = "http://api.vinnu.shop";
 export default API_BASE_URL;
